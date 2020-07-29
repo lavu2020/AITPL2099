c9720f9a7e0fefbfde7d43bf6ef0303fd6128d94
lavanya <2020lavanya@gmail.com>
2020-07-29 16:09:05 +0530

    commit

---
+++ demo1.py
@@ -0,0 +1,24 @@
+print("hello world")
+print('hello world')
+print(""hello world"")#we should not use 2 double quotation
+print(''hello world'')#we should not use double single quotation
+print("""hello world""")
+print('''hello world''')
+print("""hello
+ world
+ india""")
+print("hello\nworld\nindia")
+print("hello","world","ïndia")
+print("hello","world","ïndia",sep=",")#separating the words with comma
+print("hello","world",end=".")#ending the line with dot
+print("hello world","bangalore","india",sep=", \n",end=".")#separating the words with comma and ending the line with dot
+x=70
+print(x)
+print("x value is:",x)
+print("x value is:" + x) #we cant use + sign between two different datatypes
+print("x value is:" + str(x))  #string is type conversion
+x="hello  "
+y="world"
+z=70
+print(x+y,z)     #adding two strings
+print(x+y+str(z))
