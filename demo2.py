f52ceca6f9d34e41ef96ae7764cbdba6d384d43d
lavanya <2020lavanya@gmail.com>
2020-07-29 20:37:11 +0530

    commit

---
+++ demo2.py
@@ -0,0 +1,24 @@
+x=[]    #empty list
+l1=["hello", "world"]       #list of strings
+l2=[1,2,3,4]    #list of integers
+l3=[[1,2],[3,4]]   #list of list
+l4=[1,'hello',20,1.24]  #list of different datatypes
+print(x)
+print(l1)
+print(l2)
+print(l3)
+print(l4)
+print(len(l2))  #finds length of list
+print(len(l1))
+l2.append(5)   #adding value to the list2
+print(l2)
+l2.insert(2,10)   #inserting the value 10 at position 2
+print(l2)
+l5=[10,20,30,40,50,60]
+l5.remove(60)  #removes the value in the given list
+print(l5)
+l5.remove(70)
+print(l5)#error because 70 is nt there in the given list
+
+
+
