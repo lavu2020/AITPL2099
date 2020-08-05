d8a66e0afb15a4a8f6957b105f73b3401d5e3b2e
lavanya <2020lavanya@gmail.com>
2020-08-05 19:23:31 +0530

    commit

--- prg2.py
+++ prg2.py
@@ -2,7 +2,7 @@
 rows=5
 for row in range(1,rows):
    for column in range(row,0,-1):
-        print(column,end=' ')
+       print(column,end=' ')
     print(" ")
 #program to find second and fourth largest number
 m=int(input("enter number of elements:"))
@@ -16,3 +16,10 @@ a=list(map(int,input(" enter the numbers:").strip().split()))
 a.sort()
 print(a)
 print("3rd large number is {0},5th large number{1}".format(a[-3],a[-5]))
+#program third and fifth smallest in th given list
+m=int(input("enter number of elements:"))
+a=list(map(int,input(" enter the numbers:").strip().split()))
+a.sort()
+print(a)
+print("3rd smallest number is {0},5th smallest number{1}".format(a[3],a[5]))
+
