9210c610e6126dfd4a3525f47630d113366fd2a9
lavanya <2020lavanya@gmail.com>
2020-08-05 17:06:30 +0530

    commit

--- prg2.py
+++ prg2.py
@@ -4,3 +4,14 @@ for row in range(1,rows):
    for column in range(row,0,-1):
         print(column,end=' ')
     print(" ")
+#program to find second and fourth largest number
+L=[10,20,30,40]
+l=list(map(int(input().split())))
+l.sort()
+print(l)
+print("2nd large number is {0},4th large number{1}".format(l[-2],[-4]))
+#program third and fifth largest number
+l=list(map(int(input().split())))
+l.sort()
+print(l)
+print("2nd large number is {0},4th large number{1}".format(l[-3],[-5]))
