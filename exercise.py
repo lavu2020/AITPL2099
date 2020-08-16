02ce4a44729e01ff2bcc5d07a45c79bad02d798d
lavanya <2020lavanya@gmail.com>
2020-08-16 19:53:12 +0530

    commit

---
+++ exercise.py
@@ -0,0 +1,12 @@
+#perform 2x3,3x2 matrix arithmetic,subraction,multiplication
+import numpy as np
+a=np.array([[1,2,3],[4,5,6]])
+print(a)
+b=np.array([[3,2,1],[6,5,4]])
+print(b)
+print("....")
+print(a+b)
+print("....")
+print(a-b)
+print("....")
+print(a*b)
